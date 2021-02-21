007a3644026f5813925a8078da70b9adf03c8cbb
Charles Naut <cpn585@gmail.com>
2021-02-21 10:31:15 -0500

    Clean up extension css

--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  chrome.storage.sync.get(["firstName", "lastName", "email"], ({firstName, lastName, email}) => {
+    console.log(firstName)
+    console.log(lastName)
+    if (firstName && lastName && email) {
+      hideSetupDiv();
+      showSummaryDiv();
+    }
+  });
+
+  var saveButton = document.getElementById('save-email');
+  saveButton.addEventListener('click', function() {
+    hideSetupDiv();
+
+    chrome.storage.sync.set({
+      'firstName': document.getElementById("first").value,
+      'lastName': document.getElementById("last").value,
+      'email': document.getElementById("email").value
+    }, function() {
+      showSummaryDiv();
+    });
+  }, false);
+
   var summarizeButton = document.getElementById('summarize');
+
   summarizeButton.addEventListener('click', function() {
     chrome.tabs.getSelected(null, function(tab) {
-      fetch('http://localhost:3000?url='+tab.url).then(r => r.text()).then(result => {
+      chrome.storage.sync.get(["firstName", "lastName", "email"], ({firstName, lastName, email}) => {
+        console.log(email)
+        fetch('https://hooks.zapier.com/hooks/catch/5584995/opjk997', {
+          method: 'POST',
+          body: JSON.stringify({ firstName, lastName, email, url: tab.url, name: document.getElementById("video").value })
+        }).then(function() {
+          hideSummaryDiv();
+          showDoneDiv();
+        })
+      });
+      /**
+       *       .then(r => r.text()).then(result => {
         // Result now contains the response text, do what you want...
         console.log(result)
         
@@ -12,7 +47,32 @@ document.addEventListener('DOMContentLoaded', function() {
         f.innerHTML = result
         d.body.appendChild(f);
       });
+       */
+
     })
 
   }, false);
 }, false);
+
+function hideSetupDiv() {
+  var setupDiv = document.getElementById('setup-div');
+  setupDiv.style.display = "none";
+}
+
+function showSummaryDiv() {
+  var summarizeDiv = document.getElementById('summarize-div');
+  summarizeDiv.style.display = "block";
+}
+
+
+function hideSummaryDiv() {
+  var summarizeDiv = document.getElementById('summarize-div');
+  summarizeDiv.style.display = "none";
+}
+
+function showDoneDiv() {
+  var summarizeDiv = document.getElementById('done-div');
+  summarizeDiv.style.display = "block";
+}
+
+
